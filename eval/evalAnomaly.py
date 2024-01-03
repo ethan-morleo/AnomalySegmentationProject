e606058b0b63b0d75e2a8c9f9c95fbd15c5c10eb
Ethan Morleo <95939583+ethan-morleo@users.noreply.github.com>
2024-01-03 15:35:42 +0100

    Update evalAnomaly.py
    changing default location of erfnet  model

--- eval/evalAnomaly.py
+++ eval/evalAnomaly.py
@@ -36,7 +36,7 @@ def main():
     )  
     parser.add_argument('--loadDir',default="../trained_models/")
     parser.add_argument('--loadWeights', default="erfnet_pretrained.pth")
-    parser.add_argument('--loadModel', default="erfnet.py")
+    parser.add_argument('--loadModel', default="./models/erfnet.py")
     parser.add_argument('--subset', default="val")  #can be val or train (must have labels)
     parser.add_argument('--datadir', default="/home/shyam/ViT-Adapter/segmentation/data/cityscapes/")
     parser.add_argument('--num-workers', type=int, default=4)
